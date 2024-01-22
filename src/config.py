d7bfb2b283a9dfc7d98ee0ed8d207b869be56a11
Lili Gao <uenhk@student.kit.edu>
2024-01-22 17:59:04 +0100

    add weight decay to parameters

--- src/config.py
+++ src/config.py
@@ -11,6 +11,7 @@ parser.add_argument('--backbone', type=str, default='b5') ## b1-b5 for segformer
 parser.add_argument('--epochs', type=int, default=250)  # gpu: 250, local:1
 parser.add_argument('--lr', type=float, default=6e-5) # gpu: default=6e-5, local:2e-1
 parser.add_argument('--lr_factor', type=int, default=1)
+parser.add_argument('--weight_decay', type=float, default=0.01)
 parser.add_argument('--method', type=str, default='SegFormer') #Mask2Former or SegFormer or Detr
 parser.add_argument('--strategy', type=str, default='ddp') #ddp for 4 gpus and single for 1 gpu
 parser.add_argument('--root', type=str, default='./data/tless')
@@ -51,7 +52,7 @@ else:
 NUM_EPOCHS = args.epochs
 BACKBONE = args.backbone
 MOMENTUM = 0.9
-WEIGHT_DECAY = 0.01
+WEIGHT_DECAY = args.weight_decay
 PRECISION = '16-mixed'   #gpu: '16-mixed', cpu: bf16-mixed
 LEARNING_RATE = args.lr
 LEARNING_RATE_FACTOR = args.lr_factor
